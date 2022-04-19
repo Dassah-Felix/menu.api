0da281f4b0efb890a299f329a72414fdad598c5d
Dassah-Felix <dassahfelix12@gmailcom>
2022-04-19 14:58:36 +0000

    added user

---
+++ routes/userRoute.js
@@ -0,0 +1,9 @@
+const {Router} = require("express")
+const { createUser} = require("../controllers/userController")
+
+
+const router =Router()
+
+router.route("/users").post(createUser);
+
+module.exports=router
